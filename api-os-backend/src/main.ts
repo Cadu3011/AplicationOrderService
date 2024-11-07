a625fa4f2a604163f6d399f84a0fd275d4be0f7e
Cadu3011 <cadulinker132@gmail.com>
2024-11-06 22:49:30 -0300

    commit

--- api-os-backend/src/main.ts
+++ api-os-backend/src/main.ts
@@ -4,6 +4,14 @@ import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+
+  // Ativar CORS
+  app.enableCors({
+    origin: '*', // Substitua pelo domínio de seu frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
   app.useGlobalPipes(new ValidationPipe)
   await app.listen(3000);
 }
